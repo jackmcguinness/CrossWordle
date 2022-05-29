d5de0769012c903b10a0f98f1bf960ac7b9a958d
Jack McGuinness <>
2022-05-29 09:17:21 +0100

    Data.gd refactored for scalability

--- Scripts/Data.gd
+++ Scripts/Data.gd
@@ -4,12 +4,11 @@ var guess_n : int = 0 #gets incremented for each guess
 var answer : String = ""
 
 func _ready():
-	#Set answer to random word.
-	#(Currently sets to 5-letter word -- this needs rewriting to be scalable)
+	#Set answer to random word - current manually set to length 5 but this will
+	#change to variable for CrossGrid to call when implemented 
+	answer = generate_answer(5)
 	
-	answer = generate_answer()
-	
-	#Capitalises answer to match capitalised letter inputs.
+	#Capitalises answer to match capitalised letter inputs
 	answer = answer.to_upper()
 	
 	print(answer)
@@ -20,12 +19,12 @@ func get_answer_length():
 	return answer.length()
 
 
-func generate_answer():
+func generate_answer(length: int):
 	#Read array of n letter words from Data file
-	var word_array = read_word_file()
+	var word_array = read_word_file(length)
 	
 	#Set up random number
-	var rng = RandomNumberGenerator.new()
+	var rng = RandomNumberGenerator.new() 
 	rng.randomize()
 	var random_i = rng.randf_range(0, word_array.size())
 	
@@ -33,16 +32,18 @@ func generate_answer():
 	return word_array[random_i]
 
 
-func read_word_file():
-	#Currently reads 5 letter word
-	#Needs scaling to read from various 'n letter dict' files depending on
-	# required n value
+func read_word_file(length: int):
+	#This is a bit of a janky solution and needs refactoring as it relies each 
+	#n-letter word file existing, which we can't be sure of at the moment.
+	#Ideally, the file processing tool needs refactoring to create one single 
+	#json file containing all lengths of words, with each array of n-length words 
+	#held in an object. 
 	
-	#word_array IS NOT BEING POPULATED IN HERE
+	var file_name = str(length) + " Letter Dict.json"
 	
 	var word_array = []
 	var file = File.new()
-	file.open("res://Data/Word files/5 Letter Dict.json", file.READ)
+	file.open("res://Data/Word files/" + file_name, file.READ)
 	var text = file.get_as_text()
 	file.close()
 	var data_parse = JSON.parse(text)
